fe53c009a60acce380792c514fa424ee71ee65fd
GustavoCortezMain <d2gustavocortez@gmail.com>
2024-03-27 00:00:24 -0500

    Commit-Gustavo-2-StudentController

--- src/main/java/com/tecsup/lab03/controller/StudentController.java
+++ src/main/java/com/tecsup/lab03/controller/StudentController.java
@@ -5,6 +5,7 @@ public class StudentController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola colaboradores");
+		System.out.println("Hola soy cortez");
 	}
 
 }
