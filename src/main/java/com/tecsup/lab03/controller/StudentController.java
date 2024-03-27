2729fb4db1acb5b778bf45ba42a95160da34a9ec
jemal <jemalxd8@gmail.com>
2024-03-27 00:00:31 -0500

    escribi en la linea 9 hola soy jemal

--- src/main/java/com/tecsup/lab03/controller/StudentController.java
+++ src/main/java/com/tecsup/lab03/controller/StudentController.java
@@ -5,6 +5,8 @@ public class StudentController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola colaboradores");
+		
+		System.out.println("Hola soy jemal");
 	}
 
 }
